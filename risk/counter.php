7f52907bd646fb73a5da659a872e4bfd8689ec23
kael <kprinze25@gmail.com>
2026-02-21 11:41:11 +0800

    Implement view counter in counter.php

--- risk/counter.php
+++ risk/counter.php
@@ -1,1 +1,15 @@
+<?php
+header('Content-Type: application/json');
 
+$counter_file = 'counter.txt';
+
+if (!file_exists($counter_file)) {
+    file_put_contents($counter_file, '0');
+}
+
+$count = (int)file_get_contents($counter_file);
+$count++;
+file_put_contents($counter_file, $count);
+
+echo json_encode(['views' => number_format($count)]);
+?>
